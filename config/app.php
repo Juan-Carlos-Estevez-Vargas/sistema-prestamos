f50a79ad2ee27c42ee30e4cc52973ca2b4a920d6
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2022-08-14 15:36:25 -0500

    Archivos de configuración globales de la aplicación y de conexión a la base de datos y encriptación de contraseñas.

---
+++ config/app.php
@@ -0,0 +1,5 @@
+<?php
+    const SERVERURL = "http://localhost/prestamos/";
+    const CAMPANY = "SISTEMA PRÉSTAMO";
+    const MONEDA = "$";
+    date_default_timezone_set("America/Bogota");
---
+++ config/server.php
@@ -0,0 +1,9 @@
+<?php
+    const SERVER = "localhost";
+    const DB = "prestamos";
+    const USER = "root";
+    const PASS = "";
+    const SGBD = "mysql:host=".SERVER.";dbname=".DB;
+    const METHOD = "AES-256-CBC";
+    const SECRET_KEY = '$prestamos@2022';
+    const SECRET_ID = '037970';
