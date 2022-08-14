342cae4427cde644275d02e8e42970fb17132257
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2022-08-14 17:40:48 -0500

    Configuración de la plantilla base (template genérico) para el proyecto MVC.

--- config/app.php
+++ config/app.php
@@ -1,5 +1,5 @@
 <?php
     const SERVERURL = "http://localhost/prestamos/";
-    const CAMPANY = "SISTEMA PRÉSTAMO";
+    const COMPANY = "SISTEMA PRÉSTAMO";
     const MONEDA = "$";
     date_default_timezone_set("America/Bogota");
