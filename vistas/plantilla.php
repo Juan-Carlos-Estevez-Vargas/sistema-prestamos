c30c25c41934f32dec5362d1b173548df252e4f4
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2022-08-15 12:00:38 -0500

    Modificación del proyecto, adición de vistas a la lista de vistas que se pueden acceder y cambio de algunas vistas html a php.

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -7,9 +7,23 @@
       content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
     />
     <title><?php echo COMPANY; ?></title>
-    <?php include "./vistas/inc/link.php"; ?>
+	  <?php include "./vistas/inc/Link.php"; ?>
   </head>
   <body>
+
+    <?php
+      $peticionAjax = false;
+      require_once "./controladores/vistasControlador.php";
+      $IV = new vistasControlador();
+
+      $vistas = $IV->obtener_vistas_controlador();
+
+      if ( $vistas == "login" || $vistas == "404" ) {
+        require_once "./vistas/contenidos/".$vistas."-view.php";
+
+      } else {
+    ?>
+
     <!-- Main container -->
     <main class="full-box main-container">
       <!-- Nav lateral -->
@@ -17,9 +31,17 @@
 
       <!-- Page content -->
       <section class="full-box page-content">
-        <?php include "./vistas/inc/navBar.php"; ?>
+        <?php 
+          include "./vistas/inc/navBar.php"; 
+          include $vistas;  
+        ?>
       </section>
     </main>
-    <?php include "./vistas/inc/script.php"; ?>
+
+    <?php 
+      }
+      include "./vistas/inc/script.php"; 
+    ?>
+
   </body>
 </html>
